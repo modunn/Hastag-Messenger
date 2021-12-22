7a031dde14e7927aeea2f68d62bb95e5e0b73dd5
modunn <thasv0279@gmail.com>
2021-12-23 00:19:28 +0700

    update

--- Hastag-Msg/content.js
+++ Hastag-Msg/content.js
@@ -18,16 +18,22 @@ chrome.runtime.sendMessage({ name: "getData", user_id: getCookie('c_user') }, fu
 
 
 
-var css = '@media screen and (max-width: 900px) {\
-            .hastag_msg{\
-                width:56px;\
-                height:30px;\
-                border-radius:15px\
-            }\
-            .tag_name {display:none}\
-            }';
+
+
 var style = document.createElement('style')
-style.innerText = css
+style.innerText = '\
+.edit_tagname:focus{\
+    outline: 1px solid #C7EEFF;\
+    box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;\
+}\
+@media screen and (max-width: 900px) {\
+    .hastag_msg{\
+        width:56px;\
+        border-radius:15px\
+    }\
+    .tag_name {display:none}\
+    .edit_tag {display:none}\
+    }';
 style.rel = 'stylesheet';
 style.type = 'text/css'
 
@@ -43,9 +49,10 @@ function createTag(parent_tag) {
                         z-index:999999;\
                         margin: 0 10px;\
                         min-width:56px;\
-                        min-height:30px;\
-                        background-color:#5AD539 ;\
-                        border-radius:20px;\
+                        max-widht:324px;\
+                        min-height:20px;\
+                        background-color:transparent;\
+                        border-radius:10px;\
                         border:none;\
                         align-items: center;\
                         justify-content: space-between;\
@@ -59,12 +66,12 @@ function createTag(parent_tag) {
     var notes = document.createElement('label')
     notes.className = 'tag_name'
     notes.style.cssText = '\
-                        max-width:240px;\
-                        font-size: 13px;\
+                        max-width:225px;\
+                        font-size: 12px;\
                         font-weight: 500;\
                         color: white;\
                         position: relative;\
-                        padding:5px 30px 5px 20px;'
+                        padding:0px 15px 2px 15px;'
 
                             
     let textInt = setInterval(()=>{
@@ -85,8 +92,17 @@ function createTag(parent_tag) {
     //add nút sửa ghi chú vào tag
     var edit_tag = document.createElement('button')
     edit_tag.className = 'edit_tag'
-    edit_tag.style.cssText = 'padding-bottom:5px;border-radius:15px;width:30px;height:30px;text-align:center'
-    edit_tag.innerText = '✍🏻'
+    edit_tag.style.cssText = 'border-radius:10px;\
+                            width:50px;\
+                            height:20px;\
+                            font-size:12px;\
+                            padding-bottom:2px;\
+                            background-color:black;\
+                            color:white;\
+                            border:none;\
+                            z-index:1;\
+                            font-weight:bold;'
+    edit_tag.innerText = 'Edit'
     edit_tag.style.cursor = 'pointer'
 
 
@@ -104,18 +120,22 @@ function createTag(parent_tag) {
 }
 
 function createPopupEditTag(tag = null) {
+
+
     var edit_popup = document.createElement('div')
 
     edit_popup.id = tag.id.replace('tag_','')
     edit_popup.className = 'edit_popup'
     edit_popup.style.cssText = '\
-                    width: 250px; \
-                    height: 70px; \
+                    width: 350px; \
+                    height: 90px; \
                     background-color: rgb(255, 255, 255); \
                     position: absolute; \
-                    border-radius:2px; \
-                    border-top:3px solid #0084FF;\
-                    display: flex; \
+                    border-radius:5px; \
+                    display: flex;\
+                    flex-direction:column;\
+                    padding:15px 0;\
+                    background-color:#f3f3f5; \
                     text-align: center; \
                     align-items: center; \
                     justify-content: space-evenly;\
@@ -124,8 +144,7 @@ function createPopupEditTag(tag = null) {
 
     var pos = tag.getBoundingClientRect()
     var x = pos.width + 3
-    var y = pos.top - 67
-    console.log(x, y)
+    var y = pos.top - 107
     edit_popup.style.top = y + 'px'
     edit_popup.style.left = x + 'px'
 
@@ -135,7 +154,7 @@ function createPopupEditTag(tag = null) {
     edit_tagname.className = 'edit_tagname'
     edit_tagname.id = 'edit_tagname'
     // edit_tagname.maxLength ='35'
-
+    edit_tagname.placeholder ='Nhập nội dung cần ghi chú'
     edit_tagname.value = tag.querySelector('label').innerHTML
     edit_tagname.addEventListener("input", (e) => {
         tag.querySelector('label').innerHTML = e.target.value;
@@ -143,20 +162,40 @@ function createPopupEditTag(tag = null) {
 
 
     edit_tagname.style.cssText = 'z-index:99999;\
-                                width: 180px;\
                                 height: 30px;\
+                                width:80%;\
                                 padding: 0px;\
-                                border-radius: 5px;\
+                                border-radius: 15px;\
                                 box-sizing: border-box;\
-                                border:1px solid #0084FF;\
-                                padding: 5px;'
+                                border:none;\
+                                ouline:none;\
+                                padding: 5px 10px;'
     edit_popup.appendChild(edit_tagname)
+
+    var div_color = document.createElement('div')
+    div_color.style.cssText ='background-color: transparent; min-width: 80%; height: 30px; display: flex; justify-content: space-between;'
+
+    var defaut_color = ['#ab68ca','#3a58f0','#d62f45','#2ebf5e','#fcba03']
+
+    for (color of defaut_color) {
+        var btn_color = document.createElement('button')
+        btn_color.style.cssText = 'width:30px;height:30px;border:none;border-radius:4px;'
+        btn_color.style.backgroundColor = color 
+        btn_color.id = color
+        btn_color.onclick = function() {
+            var pick_color = this.id
+            tag.style.backgroundColor = pick_color
+            document.getElementById('pick_color').value = pick_color
+        }
+        
+        div_color.appendChild(btn_color)
+    }
 
     var color_tag = document.createElement('input')
     color_tag.type = "color"
     color_tag.id = "pick_color"
     color_tag.className = 'pick_color'
-    color_tag.style.cssText = 'height:30px;width:30px'
+    color_tag.style.cssText = 'height:30px;width:30px;border-radius:4px'
 
     color = tag.style.backgroundColor
     color_tag.value = '#' + color.substr(4, color.indexOf(')') - 4).split(',').map((color) => String("0" + parseInt(color).toString(16)).slice(-2)).join('');
@@ -167,8 +206,10 @@ function createPopupEditTag(tag = null) {
 
 
 
-    edit_popup.appendChild(color_tag)
+    div_color.appendChild(color_tag)
+    edit_popup.appendChild(div_color)
 
+    
     document.body.appendChild(edit_popup)
 
 
@@ -213,8 +254,10 @@ window.addEventListener("load", (event) => setInterval(function () {
 
 window.addEventListener('mouseup', e => {
     var close_popup = document.getElementsByClassName('edit_popup')[0]
-
-    if (close_popup && e.target != close_popup && e.target.parentNode != close_popup) {
+    if (close_popup){
+        var close_popup_tiny = close_popup.querySelector('div')
+    }
+    if (close_popup && e.target != close_popup && e.target.parentNode != close_popup &&  e.target.parentNode != close_popup_tiny) {
         close_popup.remove()
         var note = close_popup.querySelector('input').value
         var color = close_popup.querySelector('#pick_color').value
