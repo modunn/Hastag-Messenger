27256097526211632c5ef25af3cf6b81da08589c
modunn <thasv0279@gmail.com>
2022-01-03 00:38:34 +0700

    update

--- Api-server/website/auth.py
+++ Api-server/website/auth.py
@@ -23,7 +23,7 @@ def login():
         if user:
             if password == user.pass_word:
                 login_user(user, remember=True)
-                return redirect(request.args.get('next') or url_for('views.index'))
+                return redirect(request.args.get('next') or url_for('views.home'))
 
     return render_template('login.html')
 
