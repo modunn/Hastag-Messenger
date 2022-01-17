9e7502e0872dd70fe94370f91900afc3fccfb977
modunn <thasv0279@gmail.com>
2022-01-18 00:35:14 +0700

    ok

--- Api-server/website/__init__.py
+++ Api-server/website/__init__.py
@@ -8,17 +8,6 @@ from flask_socketio import SocketIO
 
 #khởi tạo trình login bằng social
 oauth = OAuth()
-oauth.register(
-    name='facebook',
-    client_id=u"212051767801881",
-    client_secret=u"269b1d496a13c53269b2de9495d1a81d",
-    access_token_url='https://graph.facebook.com/oauth/access_token',
-    access_token_params=None,
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    authorize_params=None,
-    api_base_url='https://graph.facebook.com/',
-    request_token_params={"scope": "email", "auth_type": "reauthenticate"}
-)
 
 #khởi tạo databsse
 db = SQLAlchemy()
--- Api-server/website/config/__init__.py
+++ Api-server/website/config/__init__.py
@@ -3,11 +3,10 @@ import os
 class Config(object):
     # SQLALCHEMY_DATABASE_URI         = 'sqlite:///database.sqlite'
     SQLALCHEMY_DATABASE_URI         = "postgresql://afmvhquxupwegt:8cb2f833e3e29732a81ba994be30b4cd57fae4dc0c2a3b8025fa06eeb96fe0d8@ec2-3-227-15-75.compute-1.amazonaws.com:5432/deuvmjj0t40gmv"
-    ALLOWED_EXTENSIONS              = set(['png', 'jpg', 'jpeg'])
-    UPLOAD_FOLDER                   = '/uploads/avartar'
-    SECRET_KEY                      = "mundo8888"
+    SECRET_KEY                      = "8888"
     SQLALCHEMY_TRACK_MODIFICATIONS  = False
     EXECUTOR_PROPAGATE_EXCEPTIONS   = True
     JSON_AS_ASCII                   = False
+    SESSION_COOKIE_NAME             = "mundo"
 
     
